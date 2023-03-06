02dac421c79ba5b6b47097a7b2436d2c0214d58d
geniusLAT <traber73@yandex.ru>
2023-03-06 09:15:29 +0500

    Добавил первый код, который оказался не верным

---
+++ RK/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace RK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plot_h1 = new System.Windows.Forms.PictureBox();
+            this.plot_h2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.epsilon_box = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.plot_h1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plot_h2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // plot_h1
+            // 
+            this.plot_h1.Location = new System.Drawing.Point(541, 27);
+            this.plot_h1.Name = "plot_h1";
+            this.plot_h1.Size = new System.Drawing.Size(1125, 378);
+            this.plot_h1.TabIndex = 2;
+            this.plot_h1.TabStop = false;
+            // 
+            // plot_h2
+            // 
+            this.plot_h2.Location = new System.Drawing.Point(541, 452);
+            this.plot_h2.Name = "plot_h2";
+            this.plot_h2.Size = new System.Drawing.Size(1406, 553);
+            this.plot_h2.TabIndex = 3;
+            this.plot_h2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "эпсилон";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(538, 421);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "h2";
+            // 
+            // epsilon_box
+            // 
+            this.epsilon_box.Location = new System.Drawing.Point(375, 35);
+            this.epsilon_box.Name = "epsilon_box";
+            this.epsilon_box.Size = new System.Drawing.Size(114, 20);
+            this.epsilon_box.TabIndex = 6;
+            this.epsilon_box.Text = "0,0001";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(538, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "h";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1505, 830);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.epsilon_box);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.plot_h2);
+            this.Controls.Add(this.plot_h1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.plot_h1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plot_h2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox plot_h1;
+        private System.Windows.Forms.PictureBox plot_h2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox epsilon_box;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RK/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading; //Именно это пространство имен поддерживает многопоточность
+
+namespace RK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<Point> Line(double ym, double xm, double h)
+        {
+            List<Point> points = new List<Point>();
+            //оно рисуется на промежутке от 0 до 0.5
+            int n = (int)(0.5f / h);
+
+
+            //label1.Text += "\n old_x" + xm.ToString();
+
+            double old_y = ym;
+            double old_x = xm;
+
+            float koef_x = 900 / 0.5f; //Я забыл, что промежуток на котором мы строим функцию константа
+
+
+            for (int i = 0; i < n; i++)
+            {
+                points.Add(new Point((int)(old_x * koef_x), (int)(400 - old_y * 100 * 5)));
+                old_y = GetNextY(old_y, old_x, h);
+                old_x += h;
+
+
+                //points.Add(new Point((int)(old_x* koef_x), (int)(400-old_y*100* 5)));
+
+                //400 высота поля для рисования графика, там начинается подсчёт сверху, но нам привычнее, чтобы он начинался снизу
+
+            }
+
+            return points;
+        }
+
+        public void DrawPlot(PictureBox box, List<Point> points)
+        {
+
+
+
+            Graphics graphics_h = box.CreateGraphics();
+            Pen pen = new Pen(Color.Blue, 3f);
+
+
+
+
+
+            graphics_h.DrawLines(pen, points.ToArray());
+        }
+
+
+        double my_func(double x, double y)
+        {
+            //Из подсказки на доске следует, что f(y,x) - это y'
+
+            return x * x + y * y;
+        }
+
+
+        double GetNextY(double ym, double xm, double h)
+        {
+
+            //По какой-то причине в примере представлено несколько способов, но не указано в каких
+            // случаях их допустимо применять, так что видимо можно взять любой (в них формулы для кашек отличаются)
+
+            double k0 = h * my_func(xm, ym);
+            double k1 = h * my_func(xm + h / 2, ym + k0 / 2);
+            double k2 = h * my_func(xm + h / 2, ym + k1 / 2);
+            double k3 = h * my_func(xm + h, ym + k2);
+
+            double delta_ym = (1.0f / 6.0f) * (k0 + 2 * k1 + 2 * k2 + k3);
+
+            double next_y = ym + delta_ym;
+            return next_y;
+        }
+
+        private void Calculate()
+        {
+            //y'=x^2+y^2
+            //y(0)=0.4       x принадлежит [0,1]
+
+            //для начала мы ище нужные нам k
+            double h = 0.1f;
+
+            //Судя по примеру k0=h*f(xm,ym)
+
+            //Судя по примеру xm и ym это значения из частного кейса ym=0.4f, xm = 0
+
+            double ym = 0.4f;
+            double xm = 0;
+
+
+
+            double epsilon = 0.00001;
+            try
+            {
+                epsilon = Convert.ToDouble(epsilon_box.Text);
+                label1.Text += "epsilon=" + epsilon_box.Text;
+            }
+            catch
+            {
+                label1.Text += "default epsilon=" + epsilon_box.Text;
+            }
+
+
+            int count = 50;
+            bool NotEnoughAccurate = true;
+            while (NotEnoughAccurate && count > 0)
+            {
+                //Чтобы нельзя было мататься вечно в цикле
+                count--;
+                /*Пример снабжает меня не полной информацией, но судя по всему
+                 
+                 сначала мы вычисляем дельта игрек для нормального шага и ym и xm
+
+                Потом мы берём и считаем дельта игрек для нормального шага и
+                вместо ym берём y1 (который считается из предыдущего дельта игрек),
+                откуда взялась замена xm в примере не до конца понятно. Но заметно, что новый x совпадает
+                с h. Надеюсь, что это именно часть алгоритма, а не простое совпадение. 
+
+                P.S. Я понял почему так позднее.
+
+
+                Третий подсчёт ведётся с удвоенным шагом, использует ym и xm. Что 
+                интересно, тут уже не используется никакой y2. А результат второго подсчёта с h сравнивается с результатом первого с h2
+                 
+                 */
+                h = h / 2.0f;
+
+                double y1_h1 = GetNextY(ym, xm, h);
+                label1.Text += "\n" + h.ToString() + " ->     y1_h1=" + y1_h1.ToString();
+                double y2_h1 = GetNextY(y1_h1, h, h);
+                label1.Text += "\n" + h.ToString() + " ->     y2_h1=" + y2_h1.ToString();
+
+                double y1_h2 = GetNextY(ym, xm, 2 * h);
+                label1.Text += "\n" + h.ToString() + " ->     y1_h2=" + y1_h2.ToString();
+
+                double contrast = y1_h2 - y2_h1;
+                if (contrast < 0) contrast *= -1;
+
+                label1.Text += "\n\n contrast=" + contrast.ToString();
+
+                if (contrast < epsilon)
+                {
+                    NotEnoughAccurate = false;
+                }
+
+            }
+            if (count < 1)
+            {
+                label1.Text += "too much";
+            }
+
+            //Что интересно первый график из-за другого шага выглядит длинее. Кажется там не прорисовывается последняя точка.
+            //Будь это комерческий проект, я бы занёс это в баг трекер
+
+
+
+            //void f1()
+            //{
+
+            //}
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            
+
+            //Thread myThread = new Thread(f1); //Создаем новый объект потока (Thread)
+            //myThread.Start();
+            DrawPlot(plot_h1, Line(ym, xm, h));
+            DrawPlot(plot_h2, Line(ym, xm, 2 * h));
+            stopwatch.Stop();
+
+            label1.Text += "\n" + stopwatch.ElapsedMilliseconds.ToString() + " ms";
+
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
+            label1.Text = "Count\n";
+            Calculate();
+        }
+    }
+}
